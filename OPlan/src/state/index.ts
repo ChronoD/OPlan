5997547625c45cb2ed431d2bd6663174cfde457e
AndrejusN <andrejus.n@gmail.com>
2025-03-23 22:02:59 +0200

    multiple outlines

--- OPlan/src/state/index.ts
+++ OPlan/src/state/index.ts
@@ -1,5 +1,5 @@
 import { ActionTypes, Actions } from "./actions";
-import { NormalizedOutline, OPlanState } from "./types";
+import { NormalizedOutline, OPlanState, OutlineMap } from "./types";
 
 function buildNewOutline(id: string): NormalizedOutline {
   return {
@@ -23,23 +23,36 @@ function buildNewOutlineWithChild(
 }
 
 export const initialState: OPlanState = {
-  title: "Title",
   outlines: {
-    ["0"]: buildNewOutlineWithChild("0", "01"),
-    ["01"]: buildNewOutline("01"),
+    ["1"]: buildNewOutline("1"),
+    // ["1"]: buildNewOutline("1"),
+    // ["3"]: buildNewOutlineWithChild("3", "31"),
+    // ["31"]: buildNewOutline("31"),
   },
   showXml: true,
+  topOutlineOrder: [1],
+  title: null,
 };
 
-function calculateNewId(outline: NormalizedOutline): string {
-  if (outline.items.length === 0) {
-    return (outline.id + 1).toString();
-  }
-  return (outline.id + (outline.items.length + 1)).toString();
+// function getOutlineSiblingsCount(id: string, outlines: OutlineMap) {}
+
+function calculateNewId(outlines: OutlineMap): string {
+  const outlineIds = Object.keys(outlines).sort(
+    (o1, o2) => Number(o1) - Number(o2)
+  );
+  const lastId = outlineIds.pop();
+  const nextId = lastId ? Number(lastId) + 1 : 0;
+  return nextId.toString();
 }
 
 export function reducer(state: OPlanState, action: Actions) {
   switch (action.type) {
+    case ActionTypes.TITLE_CHANGED:
+      return {
+        ...state,
+        title: action.payload.textInput,
+      };
+
     case ActionTypes.INPUT_UPDATED: {
       const outlineId = action.payload.id;
       const outlineToUpdate = state.outlines[outlineId];
@@ -58,18 +71,48 @@ export function reducer(state: OPlanState, action: Actions) {
         outlines: { ...state.outlines },
       };
     }
-    case ActionTypes.ADD_CLICKED:
-      const outlineToAddChild = state.outlines[action.payload];
-      const newId = calculateNewId(outlineToAddChild);
-      state.outlines[action.payload] = {
-        ...outlineToAddChild,
-        items: [...outlineToAddChild.items, newId],
+    case ActionTypes.ADD_SIBLING_CLICKED: {
+      const newOutlineId = calculateNewId(state.outlines);
+      const newOutline = buildNewOutline(newOutlineId);
+      state.outlines[newOutlineId] = newOutline;
+
+      const outId = action.payload.outlineId;
+      let topOrder = state.topOutlineOrder;
+      if (action.payload.parentOutlineId === null) {
+        const outlineIndex = topOrder.indexOf(Number(outId));
+        topOrder.splice(outlineIndex + 1, 0, Number(newOutlineId));
+      } else {
+        const parentOutline = state.outlines[action.payload.parentOutlineId];
+        const outlineIndex = parentOutline.items.indexOf(outId);
+        parentOutline.items.splice(outlineIndex + 1, 0, newOutlineId);
+        state.outlines[action.payload.parentOutlineId] = {
+          ...parentOutline,
+        };
+      }
+      setTimeout(() => {
+        let a = document.getElementById(newOutlineId);
+        a?.focus();
+      }, 200);
+
+      return {
+        ...state,
+        outlines: { ...state.outlines },
+        topOutlineOrder: topOrder,
       };
-      const newOutline = buildNewOutline(newId);
-      state.outlines[newId] = newOutline;
+    }
+    case ActionTypes.ADD_CHILD_CLICKED:
+      const targetOutlineId = action.payload;
+      const newOutlineId = calculateNewId(state.outlines);
+      const targetOutline = state.outlines[targetOutlineId];
+      state.outlines[targetOutlineId] = {
+        ...targetOutline,
+        items: [...targetOutline.items, newOutlineId],
+      };
+      const newOutline = buildNewOutline(newOutlineId);
+      state.outlines[newOutlineId] = newOutline;
 
       setTimeout(() => {
-        let a = document.getElementById(newId);
+        let a = document.getElementById(newOutlineId);
         a?.focus();
       }, 200);
 
@@ -77,11 +120,11 @@ export function reducer(state: OPlanState, action: Actions) {
         ...state,
         outlines: { ...state.outlines },
       };
-
     case ActionTypes.REMOVE_CLICKED: {
       const outlineId = action.payload;
       const outlineToRemove = state.outlines[outlineId];
-      if (outlineToRemove.items.length === 0) {
+      const isLastOutline = Object.values(state.outlines).length === 1;
+      if (!isLastOutline && outlineToRemove.items.length === 0) {
         delete state.outlines[outlineId];
         Object.entries(state.outlines).map(([k, v]) => {
           if (v.items.includes(outlineId)) {
